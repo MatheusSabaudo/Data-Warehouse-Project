d15db2a3a5196ee449f188bd01b4f6ba1e711526
MatheusSabaudo <matteosabaudo@outlook.it>
2026-02-22 15:26:13 +0100

    Data Quality Control added

---
+++ scripts/bronze/data_quality_check.sql
@@ -0,0 +1,29 @@
+-- QUALITY CHECK
+
+SELECT 
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id	
+HAVING COUNT(*) > 1 OR cst_id IS NULL
+
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+SELECT cst_key
+FROM bronze.crm_cust_info
+WHERE cst_key != TRIM(cst_key)
+
+-- Data Standardization& Consistency
+
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
